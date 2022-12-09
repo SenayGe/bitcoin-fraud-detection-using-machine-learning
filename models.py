2784f82b4af43663412202a218b05854a39b2b01
Senay <senaykarisful@gmail.com>
2022-12-09 22:50:54 +0400

    mionor fix

---
+++ models.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score, roc_auc_score
+import numpy as np
+
+
+def supervised_model ():
+    pass
+
+def unsupervised_model ():
+    pass
