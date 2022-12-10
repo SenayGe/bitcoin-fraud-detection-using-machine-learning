8dffb4e258cf1bd641b01588418c1fe087e19da6
Senay <senaykarisful@gmail.com>
2022-12-10 23:56:52 +0400

    minor fix

--- models.py
+++ models.py
@@ -3,8 +3,9 @@ from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_sc
 import numpy as np
 
 
-def supervised_model ():
-    pass
+def supervised_model (model, X_train, y_train):
+    model.fit(X_train, y_train)
+    return model
 
 def unsupervised_model ():
     pass
