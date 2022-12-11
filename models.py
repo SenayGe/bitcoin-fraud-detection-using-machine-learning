c60d44bf2d422b694ecfc38cb0456e733f211890
Senay <senaykarisful@gmail.com>
2022-12-11 20:31:45 +0400

    defined supervised models

--- models.py
+++ models.py
@@ -1,14 +1,27 @@
 import pandas as pd
+from xgboost import XGBClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score, roc_auc_score
 import numpy as np
 
 
 def supervised_model (model, X_train, y_train):
+    """ Use models from the sklearn library """
+
+    if model == 'RandomForest':
+        model_supervised = RandomForestClassifier()
+    elif model == 'XGBoost':
+        model_supervised = XGBClassifier()
+    elif model == 'LogisticRegression':
+        model_supervised = LogisticRegression()
+    model_supervised.fit(X_train, y_train)
+    return model_supervised
+
+def unsupervised_model (model, X_train, y_train, semi):
+    """ Use models from the pyod library """
     model.fit(X_train, y_train)
     return model
-
-def unsupervised_model ():
-    pass
 
 def neural_network ():
     pass
