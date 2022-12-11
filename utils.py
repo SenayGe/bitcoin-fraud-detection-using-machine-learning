5b2c861fd00e07bd8effa586579a52be9cff864c
Senay <senaykarisful@gmail.com>
2022-12-11 22:24:37 +0400

    model performance evaluation functions done

--- utils.py
+++ utils.py
@@ -1,3 +1,51 @@
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score, roc_auc_score
+from matplotlib import pyplot as plt
+import numpy as np
+
+def calc_performance_score (y_true, y_pred, metric='f1'):
+    performance_scores = {'accuracy': accuracy_score(y_true, y_pred), 'f1': f1_score(y_true, y_pred, pos_label=1),
+                   'f1_micro': f1_score(y_true, y_pred, average='micro'),
+                   'f1_macro': f1_score(y_true, y_pred, average='macro'),
+                   'precision': precision_score(y_true, y_pred), 'recall': recall_score(y_true, y_pred),
+                   'roc_auc': roc_auc_score(y_true, y_pred)}
+    
+    # Return a specific metric
+    model_score = performance_scores[metric]
+
+    return model_score
+
+def evaluate_performance(y_true, y_pred, metric='f1'):
+
+    model_scores = []
+    for y_i in y_pred:
+        score = calc_performance_score(y_true.astype('int'), y_i, metric)
+        model_scores.append(score)
+
+    avg_score = np.mean(model_scores)
+
+    return avg_score
+
+
+
+def average_performance_per_timestep(X_test, y_test, y_preds, metric= 'f1'):
+    """ Calculates the average performance score per timestep"""
+
+    first_test_timestep = min(X_test['time_step']) # Timestep where test data starts
+    last_time_step = max(X_test['time_step'])
+
+    all_scores = []
+    for y_pred in y_preds:
+        score_ts = [] # Score in each timestep
+        for time_step in range(first_test_timestep , last_time_step + 1):
+            time_step_idx = np.flatnonzero(X_test['timestep'] == time_step)
+            y_true_ts = y_test.iloc[time_step_idx]
+            y_pred_ts = [y_pred[i] for i in time_step_idx]
+            score_ts.append(calc_performance_score(y_true_ts.astype('int'), y_pred_ts, metric))
+        all_scores.append(score_ts)
+
+    avg_f1 = np.array([np.mean([f1_scores[i] for f1_scores in all_scores]) for i in range(15)])
+
+    return avg_f1 
 
 def plot_performance_per_timestep(model_metric_dict, last_train_time_step=34,last_time_step=49, model_std_dict=None, fontsize=23, labelsize=18, figsize=(20, 10),
                                   linestyle=['solid', "dotted", 'dashed'], linecolor=["green", "orange", "red"],
