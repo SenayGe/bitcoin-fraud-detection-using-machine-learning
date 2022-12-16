036f356a73738cece00e99b9e6b7462917015ff6
Senay <senaykarisful@gmail.com>
2022-12-16 22:59:45 +0400

    minor fix

--- utils.py
+++ utils.py
@@ -1,12 +1,14 @@
 from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score, roc_auc_score
+from data_preprocessing import load_elliptic_dataset
 from matplotlib import pyplot as plt
 import numpy as np
+import os
 
 def calc_performance_score (y_true, y_pred, metric='f1'):
     performance_scores = {'accuracy': accuracy_score(y_true, y_pred), 'f1': f1_score(y_true, y_pred, pos_label=1),
                    'f1_micro': f1_score(y_true, y_pred, average='micro'),
                    'f1_macro': f1_score(y_true, y_pred, average='macro'),
-                   'precision': precision_score(y_true, y_pred), 'recall': recall_score(y_true, y_pred),
+                   'precision': precision_score(y_true, y_pred, pos_label=1), 'recall': recall_score(y_true, y_pred),
                    'roc_auc': roc_auc_score(y_true, y_pred)}
     
     # Return a specific metric
@@ -16,39 +18,44 @@ def calc_performance_score (y_true, y_pred, metric='f1'):
 
 def evaluate_performance(y_true, y_pred, metric='f1'):
 
-    if not isinstance(y_pred,list):
-        return calc_performance_score(y_true.astype('int'), y_pred, metric)
-        
-    model_scores = []
-    for y_i in y_pred:
-        score = calc_performance_score(y_true.astype('int'), y_i, metric)
-        model_scores.append(score)
+    # if not isinstance(y_pred,list):
+    #     return calc_performance_score(y_true.astype('int'), y_pred, metric)
 
-    avg_score = np.mean(model_scores)
+    # model_scores = []
+    # for y_i in y_pred:
+    #     score = calc_performance_score(y_true.astype('int'), y_i, metric)
+    #     model_scores.append(score)
 
-    return avg_score
+    # avg_score = np.mean(model_scores)
+
+    return calc_performance_score(y_true.astype('int'), y_pred, metric)
 
 
-def average_performance_per_timestep(X_test, y_test, y_preds, metric= 'f1'):
+def average_performance_per_timestep(X_test, y_test, y_pred, metric= 'f1'):
     """ Calculates the average performance score per timestep"""
 
     first_test_timestep = min(X_test['time_step']) # Timestep where test data starts
     last_time_step = max(X_test['time_step'])
 
     all_scores = []
-    for y_pred in y_preds:
-        score_ts = [] # Score in each timestep
-        for time_step in range(first_test_timestep , last_time_step + 1):
-            time_step_idx = np.flatnonzero(X_test['timestep'] == time_step)
-            y_true_ts = y_test.iloc[time_step_idx]
-            y_pred_ts = [y_pred[i] for i in time_step_idx]
-            score_ts.append(calc_performance_score(y_true_ts.astype('int'), y_pred_ts, metric))
-        all_scores.append(score_ts)
+    # for y_pred in y_preds:
+    score_ts = [] # Score in each timestep
+    for time_step in range(first_test_timestep , last_time_step + 1):
+        time_step_idx = np.flatnonzero(X_test['time_step'] == time_step)
+        y_true_ts = y_test.iloc[time_step_idx]
+        y_pred_ts = [y_pred[i] for i in time_step_idx]
+        score_ts.append(calc_performance_score(y_true_ts.astype('int'), y_pred_ts, metric))
+    all_scores.append(score_ts)
 
     avg_f1 = np.array([np.mean([f1_scores[i] for f1_scores in all_scores]) for i in range(15)])
 
     return avg_f1 
 
+def calc_occurences_per_timestep():
+    X, y, _ = load_elliptic_dataset()
+    X['class'] = y
+    occ = X.groupby(['time_step', 'class']).size().to_frame(name='occurences').reset_index()
+    return occ
 
 
 def plot_performance_per_timestep(model_metric_dict, last_train_time_step=34,last_time_step=49, model_std_dict=None, fontsize=23, labelsize=18, figsize=(20, 10),
@@ -95,4 +102,4 @@ def plot_performance_per_timestep(model_metric_dict, last_train_time_step=34,las
     if savefig_path == None:
         plt.show()
     else:
-        plt.savefig(os.path.join(ROOT_DIR, savefig_path), bbox_inches='tight', pad_inches=0)
+        plt.savefig(savefig_path, bbox_inches='tight', pad_inches=0)
